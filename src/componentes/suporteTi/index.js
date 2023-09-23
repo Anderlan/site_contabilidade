545f010021d8bd3982a4c3ef47521d14baaa8b03
Anderlan <anderlansiqueira@gmail.com>
2023-09-23 07:18:56 -0300

    modified:   src/componentes/suporteTi/style.css

--- src/componentes/suporteTi/index.js
+++ src/componentes/suporteTi/index.js
@@ -1,30 +1,42 @@
 import React from "react";
 import "./style.css"
 
-function Services() {
-    return(
-        <div className="containerCard">
-            <div className="leftContainerCard">
-                <h1>Suporte de TI</h1>
-                <h2>Soluções Tecnológicas Sob Medida para Você! Presencial e Online</h2>
-                <p>O Suporte de TI garante o pleno funcionamento de seus sistemas e equipamentos com nosso serviço de Suporte de TI. 
-                Nossa equipe de especialistas está pronta para resolver qualquer problema, desde questões simples até desafios complexos. 
-                Mantenha sua infraestrutura tecnológica estável e produtiva com nosso suporte ágil e eficiente. Mantenha sua infraestrutura tecnológica estável 
-                e produtiva com nosso suporte ágil e eficiente.
-                </p>
+import ImgTi from "../../assets/imgleft.jpg";
 
-                <ul>
-                    <li>🔧 Montagem e Manutenção de Computadores;</li>
-                    <li>💻 Formatação de Computadores e Equipamentos Periféricos;</li>
-                    <li>💡 Consultoria em Orçamentos de Equipamentos de Tecnologia.</li>                    
-                </ul>
-            </div>
-            <div className="rightContainerCard">
-                TETSTST
+function SuporteTi() {
+    return (
+        <div className="containerService">
+            <div className="containerCard">
+
+                <div className="leftContainerCard">
+                    <h1>Suporte de TI</h1>
+                    <h2>Soluções Tecnológicas Sob Medida para Você! Presencial e Online</h2>
+                    <p>O Suporte de TI garante o pleno funcionamento de seus sistemas e equipamentos com nosso serviço de Suporte de TI.
+                        Nossa equipe de especialistas está pronta para resolver qualquer problema, desde questões simples até desafios complexos.
+                        Mantenha sua infraestrutura tecnológica estável e produtiva com nosso suporte ágil e eficiente. Mantenha sua infraestrutura tecnológica estável
+                        e produtiva com nosso suporte ágil e eficiente.
+                    </p>
+
+                    <ul>
+                        <li>🔧 Montagem e Manutenção de Computadores;</li>
+                        <li>💻 Formatação de Computadores e Equipamentos Periféricos;</li>
+                        <li>💡 Consultoria em Orçamentos de Equipamentos de Tecnologia.</li>
+                    </ul>
+
+                    <div className="botoes">
+                        <ul>    
+                            <li><a href="/" className="saibaMais">Saiba Mais</a></li>
+                            <li><a href="/" className="duvidasFrequentes">Dúvidas Frequentes</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="rightContainerCard">
+                    <img className="imagesRight" src={ImgTi} alt="ImgTi"></img>
+                </div>
+
             </div>
         </div>
-
     );
 }
 
-export default Services;
+export default SuporteTi;
