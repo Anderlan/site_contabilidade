6a9531785d7bc2720c858e953ec256016e21586c
anderlan <anderlansiqueira@gmail.com>
2023-09-25 14:44:34 -0300

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './componentes/home';
 import './App.css';
 
 function App() {
     return (
         <div className='App'>
-            <BrowserRouter>
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                </Routes>
-            </BrowserRouter>
+            <Home />
         </div>
     );
 }
